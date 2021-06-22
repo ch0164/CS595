49d8766a09d696904bc3b2be7730e976ce136caf
Christian Hall <ch0164@uah.edu>
2021-06-22 18:59:10 -0500

    Removed commented debug output.

--- WolvesSheepGrass/Source/simulation.py
+++ WolvesSheepGrass/Source/simulation.py
@@ -26,13 +26,13 @@ def run_simulation():
 
     # Set environment parameters.
     # TODO: World size can differ in x and y dimensions.
-    world_size, grass_regrowth_time = 4, 10
+    world_size, grass_regrowth_time = 25, 10
 
     # Initialize the environment.
     wsg_world = Environment(world_size, grass_regrowth_time)
 
     # Set initial conditions and parameters.
-    wolf_count, sheep_count = 1, 1
+    wolf_count, sheep_count = 20, 40
     wolf_food_gain, sheep_food_gain = 40, 4
     wolf_reproduction, sheep_reproduction = 0.05, 0.04
     max_sheep_count = 10_000
@@ -73,40 +73,6 @@ def run_simulation():
         wsg_world.cultivate()
 
         iteration += 1
-
-        # For manual simulation only -- suspend execution to examine output.
-        # time.sleep(tick)
-
-
-
-
-
-    # Used for debugging:
-
-    # for _ in range(1000):
-    #     for index, sheep in enumerate(sheep_list):
-    #         sheep.Move()
-    #         if wsg_world.terrain[sheep.row % world_size][sheep.col % world_size]:
-    #             print("Sheep {0} is on grass! Located at: {1}"
-    #                   .format(index, str((sheep.row % world_size, sheep.col % world_size))))
-    #         else:
-    #             print("Sheep {0} is on dirt... Located at: {1}"
-    #                   .format(index, str((sheep.row % world_size, sheep.col % world_size))))
-    # wsg_world.print_world()
-
-    # for _ in range(11):
-    #     for index, wolf in enumerate(wolf_list):
-    #         current_position = "(" + str(wolf.row) + ", " + str(wolf.col) + ")"
-    #         wolf.Move()
-    #         next_position = "(" + str(wolf.row) + ", " + str(wolf.col) + ")"
-    #         print("Wolf " + str(index) + ": Current Position = " + current_position + "\tNext position = " + next_position
-    #               + "\tEnergy: " + str(wolf.energy))
-    #         if wolf.energy <= 0:
-    #             wolf_list.pop(index)
-    #         if not wolf_list:
-    #             print("The wolves have all died out...")
-    #             break
-
 
 if __name__ == "__main__":
     run_simulation()
